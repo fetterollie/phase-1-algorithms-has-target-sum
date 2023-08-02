5010f3709d835eb8c578419ebc78e960a852a67d
fetterollie <jonathan.fetterolf@gmail.com>
2023-08-02 10:41:12 -0400

    problem solved and code commented

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  let works = false
+  const maxIndex = array.length - 1
+  const checkIndices = [...Array(array.length).keys()];
+  array.forEach(element => {
+
+    const elementIndex = array.indexOf(element)
+    // console.log("element:", element, "elementIndex", elementIndex)
+    checkIndices.forEach(index => {
+      // console.log("currentCheckIndex:", index)
+      if (index !== elementIndex) {
+        if (array[elementIndex] + array[index] === target) {
+          // console.log("got it")
+          works = true
+        }
+      }
+    })
+  });
+  // console.log(works)
+  return works
 }
+
+// hasTargetSum([3, 8, 12, 4, 11, 7], 10);
+// hasTargetSum([22, 19, 4, 6, 30], 25);
+// hasTargetSum([1, 2, 5], 4);
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,10 +31,13 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  does element[0] + any other element equal the target
+  return true else return false
 */
 
 /*
   Add written explanation of your solution here
+  we'll have to know how long the arary is
 */
 
 // You can run `node index.js` to view these console logs
